78132820d37314e812e2b258689cf41c136572a2
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2023-11-26 14:08:49 +0900

    ok

--- lib/twitter/twitter.ts
+++ lib/twitter/twitter.ts
@@ -1,4 +1,7 @@
-import { TwitterOpenApi, TwitterOpenApiClient } from "twitter-openapi-typescript";
+import {
+  TwitterOpenApi,
+  TwitterOpenApiClient,
+} from "twitter-openapi-typescript";
 import { env } from "../util";
 import { login } from "./login";
 import { subscriptionCollection } from "../db";
@@ -10,10 +13,10 @@ export class Twitter {
     channelId: string;
     userId: string;
     twitterUserId: string;
-  }[] = []
-  private interval = 10 * 60 * 1000
-  private timer: NodeJS.Timeout | null = null
-  constructor() { }
+  }[] = [];
+  private interval = 10 * 60 * 1000;
+  private timer: NodeJS.Timeout | null = null;
+  constructor() {}
   public async login() {
     this.client = await getApiClientFromEmailAndPassword(
       env("email"),
@@ -22,59 +25,84 @@ export class Twitter {
     );
   }
   public async getTweets(userid: string) {
-    if (this.client === null) { return; }
-    const user = await this.getUser(userid)
-    if (!user?.user) { return undefined }
-    return (await this.client.getTweetApi().getUserTweets({ "userId": user.user.restId,"count":10 })).data.data
+    if (this.client === null) {
+      return;
+    }
+    const user = await this.getUser(userid);
+    if (!user?.user) {
+      return undefined;
+    }
+    return (
+      await this.client
+        .getTweetApi()
+        .getUserTweets({ userId: user.user.restId, count: 10 })
+    ).data.data;
   }
   public async getUser(userid: string) {
-    if (this.client === null) { return undefined; }
+    if (this.client === null) {
+      return undefined;
+    }
     if (userid.match(/^[0-9]+$/) !== null) {
-      const data= (await this.client.getUserApi().getUserByRestId({ "userId":userid })).data
-      return data
-    } else if(userid.match(/^[a-zA-Z0-9_]+$/) !== null){
-      return (await this.client.getUserApi().getUserByScreenName({ "screenName": userid })).data
+      const data = (
+        await this.client.getUserApi().getUserByRestId({ userId: userid })
+      ).data;
+      return data;
+    } else if (userid.match(/^[a-zA-Z0-9_]+$/) !== null) {
+      return (
+        await this.client
+          .getUserApi()
+          .getUserByScreenName({ screenName: userid })
+      ).data;
     }
   }
   public async startTimer(client: Client) {
-    this.subscriptions = (await subscriptionCollection.find({}).toArray())
-    const exec=async () => {
-      console.log("exec")
-      this.subscriptions.forEach(async sub => {
-        const tweets = await this.getTweets(sub.twitterUserId)
-        const now = new Date()
-        tweets?.forEach(async tweet => {
+    this.subscriptions = await subscriptionCollection.find({}).toArray();
+    const exec = async () => {
+      console.log("exec");
+      this.subscriptions.forEach(async (sub) => {
+        const tweets = await this.getTweets(sub.twitterUserId);
+        const now = new Date();
+        tweets?.forEach(async (tweet) => {
           const time = tweet.tweet.legacy?.createdAt;
-          if (!time) { return }
-          const date = new Date(time)
+          if (!time) {
+            return;
+          }
+          const date = new Date(time);
           if (now.getTime() - date.getTime() < this.interval) {
-            const embed = new EmbedBuilder({
-              title: `${tweet.user?.legacy.name} (@${tweet.user?.legacy.screenName})`,
-              description: tweet.tweet.legacy?.fullText,
-              color: 0x00ffff,
-              image: {
-                url: tweet.tweet.legacy?.entities?.media?.[0]?.url ?? "",
-              },
-              url: `https://twitter.com/${tweet.user?.legacy.screenName}/status/${tweet.tweet.legacy?.idStr}`,
-              timestamp: date,
-            })
-            const channel = client.channels.cache.get(sub.channelId)
+            const embed = new EmbedBuilder()
+              .setTitle(
+                `${tweet.user?.legacy.name}さんによるツイート (@${tweet.user?.legacy.screenName})`
+              )
+              .setURL(
+                `https://twitter.com/${tweet.user?.legacy.screenName}/status/${tweet.tweet.legacy?.idStr}`
+              )
+              .setTimestamp(date)
+              .setColor(0x00ffff)
+              .setImage(
+                tweet.tweet.legacy?.entities?.media?.[0]?.mediaUrlHttps ?? ""
+              )
+              .setDescription(tweet.tweet.legacy?.fullText ?? "none")
+              .setAuthor({
+                name: tweet.user?.legacy.name ?? "none",
+                url: `https://twitter.com/${tweet.user?.legacy.screenName}`,
+                iconURL: tweet.user?.legacy.profileImageUrlHttps ?? "none",
+              });
+            const channel = client.channels.cache.get(sub.channelId);
             if (channel?.isTextBased()) {
-              channel.send({ embeds: [embed] })
+              channel.send({ embeds: [embed] });
             }
           }
-        }
-        )
-      })
-    }
-    exec()
+        });
+      });
+    };
+    exec();
     this.timer = setInterval(exec, this.interval);
   }
-  public reloadTimer(client: Client){
-    if(this.timer){
-      clearInterval(this.timer)
+  public reloadTimer(client: Client) {
+    if (this.timer) {
+      clearInterval(this.timer);
     }
-    this.startTimer(client)
+    this.startTimer(client);
   }
 }
 
