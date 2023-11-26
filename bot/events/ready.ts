1daa2627049b32d069e77e732be1fd01c45c82fd
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2023-11-26 05:17:10 +0000

    activity

--- bot/events/ready.ts
+++ bot/events/ready.ts
@@ -4,6 +4,7 @@ import { eventHandler } from "../manager/base.js"
 export const handler: eventHandler<"ready"> = {
     name: "ready",
     exec: async (bot, client) => {
+      client.user.setActivity('x!help', { type: 'WATCHING' })
         console.log(`ready as ${client.user?.tag}`)
     }
 }
