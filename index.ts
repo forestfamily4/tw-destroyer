3a4705a5a4b081342a4ecec1cff197068f86dd16
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2023-11-27 03:55:16 +0000

    asjiofsjoafpoijafwpoiasasojasjafoj

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { Server } from "./lib/server";
 import { Twitter } from "./lib/twitter/twitter";
 
 async function main() {
+  process.on("uncaughtException", (err)=>console.log(err));
   const server=new Server("http://localhost:3000")
   const bot=new Bot()
   await bot.login()
