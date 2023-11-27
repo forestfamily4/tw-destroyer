0868867e374610cba6c756d7f87303e394ab5f28
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2023-11-27 12:56:07 +0900

    a

--- lib/twitter/twitter.ts
+++ lib/twitter/twitter.ts
@@ -2,7 +2,7 @@ import { TwitterOpenApi, TwitterOpenApiClient } from "twitter-openapi-typescript
 import { env } from "../util";
 import { login } from "./login";
 import { subscriptionCollection } from "../db";
-import { Client, EmbedBuilder } from "discord.js";
+import { ActivityType, Client, EmbedBuilder } from "discord.js";
 
 export class Twitter {
   private client: TwitterOpenApiClient | null = null;
@@ -25,53 +25,58 @@ export class Twitter {
     if (this.client === null) { return; }
     const user = await this.getUser(userid)
     if (!user?.user) { return undefined }
-    return (await this.client.getTweetApi().getUserTweets({ "userId": user.user.restId,"count":10 })).data.data
+    return (await this.client.getTweetApi().getUserTweets({ "userId": user.user.restId, "count": 10 })).data.data
   }
   public async getUser(userid: string) {
     if (this.client === null) { return undefined; }
     if (userid.match(/^[0-9]+$/) !== null) {
-      const data= (await this.client.getUserApi().getUserByRestId({ "userId":userid })).data
+      const data = (await this.client.getUserApi().getUserByRestId({ "userId": userid })).data
       return data
-    } else if(userid.match(/^[a-zA-Z0-9_]+$/) !== null){
+    } else if (userid.match(/^[a-zA-Z0-9_]+$/) !== null) {
       return (await this.client.getUserApi().getUserByScreenName({ "screenName": userid })).data
     }
   }
   public async startTimer(client: Client) {
     this.subscriptions = (await subscriptionCollection.find({}).toArray())
-    const exec=async () => {
+    const exec = async () => {
       console.log("exec")
       this.subscriptions.forEach(async sub => {
-        const tweets = await this.getTweets(sub.twitterUserId)
-        const now = new Date()
-        tweets?.forEach(async tweet => {
-          const time = tweet.tweet.legacy?.createdAt;
-          if (!time) { return }
-          const date = new Date(time)
-          if (now.getTime() - date.getTime() < this.interval) {
-            const embed = new EmbedBuilder({
-              title: `${tweet.user?.legacy.name} (@${tweet.user?.legacy.screenName})`,
-              description: tweet.tweet.legacy?.fullText,
-              color: 0x00ffff,
-              image: {
-                url: tweet.tweet.legacy?.entities?.media?.[0]?.url ?? "",
-              },
-              url: `https://twitter.com/${tweet.user?.legacy.screenName}/status/${tweet.tweet.legacy?.idStr}`,
-              timestamp: date,
-            })
-            const channel = client.channels.cache.get(sub.channelId)
-            if (channel?.isTextBased()) {
-              channel.send({ embeds: [embed] })
+        try {
+          const tweets = await this.getTweets(sub.twitterUserId)
+          const now = new Date()
+          tweets?.forEach(async tweet => {
+            const time = tweet.tweet.legacy?.createdAt;
+            if (!time) { return }
+            const date = new Date(time)
+            if (now.getTime() - date.getTime() < this.interval) {
+              const embed = new EmbedBuilder({
+                title: `${tweet.user?.legacy.name} (@${tweet.user?.legacy.screenName})`,
+                description: tweet.tweet.legacy?.fullText,
+                color: 0x00ffff,
+                image: {
+                  url: tweet.tweet.legacy?.entities?.media?.[0]?.url ?? "",
+                },
+                url: `https://twitter.com/${tweet.user?.legacy.screenName}/status/${tweet.tweet.legacy?.idStr}`,
+                timestamp: date,
+              })
+              const channel = client.channels.cache.get(sub.channelId)
+              if (channel?.isTextBased()) {
+                channel.send({ embeds: [embed] })
+              }
             }
-          }
+          })
+
+        } catch (e) {
+          client.user?.setActivity("X API ERROR", { type: ActivityType.Playing })
         }
-        )
       })
+      client.user?.setActivity("x!help", { type: ActivityType.Playing })
     }
     exec()
     this.timer = setInterval(exec, this.interval);
   }
-  public reloadTimer(client: Client){
-    if(this.timer){
+  public reloadTimer(client: Client) {
+    if (this.timer) {
       clearInterval(this.timer)
     }
     this.startTimer(client)
